aae9233f7a69d9601bfddf28dff358c861547b85
John Doe <montekke@gmail.com>
2022-09-20 14:13:46 +0300

    Обновление 3.3

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-var { src, dest, watch, parallel, series } = require ('gulp');
-var sass = require ('gulp-sass')(require('sass'));
-var concat = require ('gulp-concat');
-var browserSync = require ('browser-sync').create();
-var uglify = require ('gulp-uglify-es').default;
+var { src, dest, watch, parallel, series } = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+var uglify = require('gulp-uglify-es').default;
 var autoprefixer = require('gulp-autoprefixer');
 var imagemin = require('gulp-imagemin');
 var del = require('del');
@@ -14,130 +14,130 @@ const sourcemaps = require('gulp-sourcemaps');
 let isProd = false; // dev by default
 
 function browsersync() {
-  browserSync.init({
-    server: {
-      baseDir: 'dist'
-    },
-    reloadDelay: 500
-  })
+	browserSync.init({
+		server: {
+			baseDir: 'dist'
+		},
+		reloadDelay: 500
+	})
 }
 
 function cleanDist() {
-  return del('dist')
+	return del('dist')
 }
 
 function cleanCache() {
-  return del(['dist/js'], ['dist/css'], ['dist/fonts'], ['dist/index.html'])
+	return del(['dist/js'], ['dist/css'], ['dist/fonts'], ['dist/index.html'])
 }
 
 function images() {
-  return src('app/img/**/*')
-    .pipe(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.mozjpeg({quality: 75, progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
-      imagemin.svgo({
-        plugins: [
-            {removeViewBox: true},
-            {cleanupIDs: false}
-        ]
-    })
-    ]))
-    .pipe(dest('dist/img'))
+	return src('app/_img/**/*')
+		.pipe(imagemin([
+			imagemin.gifsicle({ interlaced: true }),
+			imagemin.mozjpeg({ quality: 75, progressive: true }),
+			imagemin.optipng({ optimizationLevel: 5 }),
+			imagemin.svgo({
+				plugins: [
+					{ removeViewBox: true },
+					{ cleanupIDs: false }
+				]
+			})
+		]))
+		.pipe(dest('dist/img'))
 }
 
 function webpconvert() {
-  return src(['app/img/**/*', '!app/img/**/*.webp'])
-  .pipe(webp())
-  .pipe(dest('dist/img'))
+	return src(['app/_img/**/*', '!app/_img/**/*.webp'])
+		.pipe(webp())
+		.pipe(dest('dist/img'))
 }
 
 
 
 function images_bufer() {
-  return src('app/img/bufer/*')
-    .pipe(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.mozjpeg({quality: 75, progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
-      imagemin.svgo({
-        plugins: [
-            {removeViewBox: true},
-            {cleanupIDs: false}
-        ]
-    })
-    ]))
-    .pipe(dest('dist/img/bufer'))
+	return src('app/_img/_bufer/*')
+		.pipe(imagemin([
+			imagemin.gifsicle({ interlaced: true }),
+			imagemin.mozjpeg({ quality: 75, progressive: true }),
+			imagemin.optipng({ optimizationLevel: 5 }),
+			imagemin.svgo({
+				plugins: [
+					{ removeViewBox: true },
+					{ cleanupIDs: false }
+				]
+			})
+		]))
+		.pipe(dest('dist/img/bufer'))
 }
 function webpconvert_bufer() {
-  return src(['app/img/bufer/*', '!app/img/**/*.webp'])
-  .pipe(webp())
-  .pipe(dest('dist/img/bufer'))
+	return src(['app/_img/_bufer/*', '!app/_img/**/*.webp'])
+		.pipe(webp())
+		.pipe(dest('dist/img/bufer'))
 }
 
 const htmlInclude = () => {
-  return src(['app/*.html'])
-    .pipe(fileInclude({
-      prefix: '@',
-      basepath: '@file'
-    }))
-    .pipe(dest('dist'))
-    .pipe(browserSync.stream());
+	return src(['app/*.html'])
+		.pipe(fileInclude({
+			prefix: '@',
+			basepath: '@file'
+		}))
+		.pipe(dest('dist'))
+		.pipe(browserSync.stream());
 }
 
 const scripts = () => {
-	src('app/js/modules/**.js')
+	src('app/_js/modules/**.js')
 		.pipe(concat('modules.min.js'))
-    .pipe(uglify())
+		.pipe(uglify())
 		.pipe(dest('dist/js/'))
-  return src(
-    ['app/js/global.js', '.app/js/components/**.js', 'app/js/main.js'])
-    .pipe(gulpif(!isProd, sourcemaps.init()))
-    .pipe(uglify())
-    .pipe(concat('main.min.js'))
-    .pipe(gulpif(!isProd, sourcemaps.write('.')))
-    .pipe(dest('dist/js'))
-    .pipe(browserSync.stream());
+	return src(
+		['app/_js/global.js', '.app/_js/components/**.js', 'app/_js/main.js'])
+		.pipe(gulpif(!isProd, sourcemaps.init()))
+		.pipe(uglify())
+		.pipe(concat('main.min.js'))
+		.pipe(gulpif(!isProd, sourcemaps.write('.')))
+		.pipe(dest('dist/js'))
+		.pipe(browserSync.stream());
 }
 
 const styles = () => {
-  return src('app/scss/**/*.scss')
-    .pipe(gulpif(!isProd, sourcemaps.init()))
-    .pipe (sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(autoprefixer({
-      cascade: false,
-    }))
-    .pipe(gulpif(!isProd, sourcemaps.write('.')))
-    .pipe(dest('dist/css'))
-    .pipe(browserSync.stream());
+	return src('app/_scss/**/*.scss')
+		.pipe(gulpif(!isProd, sourcemaps.init()))
+		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(autoprefixer({
+			cascade: false,
+		}))
+		.pipe(gulpif(!isProd, sourcemaps.write('.')))
+		.pipe(dest('dist/css'))
+		.pipe(browserSync.stream());
 };
 
 const styleslight = () => {
-  return src('app/scss/style.scss')
-    .pipe(gulpif(!isProd, sourcemaps.init()))
-    .pipe (sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(autoprefixer({
-      cascade: false,
-    }))
-    .pipe(gulpif(!isProd, sourcemaps.write('.')))
-    .pipe(dest('dist/css'))
-    .pipe(browserSync.stream());
+	return src('app/_scss/style.scss')
+		.pipe(gulpif(!isProd, sourcemaps.init()))
+		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(autoprefixer({
+			cascade: false,
+		}))
+		.pipe(gulpif(!isProd, sourcemaps.write('.')))
+		.pipe(dest('dist/css'))
+		.pipe(browserSync.stream());
 };
 
 function build() {
-  return src([
-    'app/fonts/**/*',
-  ], {base: 'app'})
-  .pipe(dest('dist'))
+	return src([
+		'app/_fonts/**/*',
+	], { base: 'app' })
+		.pipe(dest('dist'))
 }
 
 function watching() {
-  watch(['app/scss/**/*.scss'], styleslight);
-  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-  watch(['app/*.html']).on('change', browserSync.reload);
-	watch('app/img/bufer/*.{jpg,jpeg,png,svg}', series(webpconvert_bufer, images_bufer));
-  watch(['app/**/*.html'], htmlInclude);
-  watch(['app/*.html'], htmlInclude);
+	watch(['app/_scss/**/*.scss'], styleslight);
+	watch(['app/_js/**/*.js', '!app/_js/main.min.js'], scripts);
+	watch(['app/*.html']).on('change', browserSync.reload);
+	watch('app/_img/_bufer/*.{jpg,jpeg,png,svg}', series(webpconvert_bufer, images_bufer));
+	watch(['app/**/*.html'], htmlInclude);
+	watch(['app/*.html'], htmlInclude);
 }
 
 exports.html = htmlInclude;
@@ -149,6 +149,6 @@ exports.images = images;
 exports.cleanDist = cleanDist;
 exports.webpconvert = webpconvert;
 
-exports.build = series(cleanDist, webpconvert, images, styles, scripts,htmlInclude, build);
+exports.build = series(cleanDist, webpconvert, images, styles, scripts, htmlInclude, build);
 exports.cache = series(cleanCache, styles, scripts, build);
-exports.default = parallel(htmlInclude, styles, scripts,  browsersync, watching);
+exports.default = parallel(htmlInclude, styles, scripts, browsersync, watching);
